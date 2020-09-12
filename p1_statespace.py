5afa9d403249b81f7acc6a4e67ad4a8b99897614
Joon Kim <jkkim796@gmail.com>
2020-09-12 18:13:21 -0500

    Joon's implementation of P1 statespace

--- p1_statespace.py
+++ p1_statespace.py
@@ -1,79 +1,82 @@
-# returns a copy of state which fills the jug corresponding to the index in which (0 or 1) to its maximum capacity
-def fill(state, maxCap, which):
 
-	# copy of state
-	origin = list(state)	
-	
-	result = origin 
-	result[which] = maxCap[which]
-	return result
-		
-# returns a copy of state which empties the jug corresponding to the index in which (0 or 1). 
-def empty(state, maxCap, which):
+def fill(state, max, which):
+    # Make a copy of state
+    state_copy = state[:]
+    state_copy[which] = max[which]
+    return state_copy
 
-	# copy of state
-	origin2 = list(state)
-	result2 = origin2
-	result2[which] = 0
-	return result2
+def empty(state, max, which):
+    # Make a copy of state
+    state_copy = state[:]
+    state_copy[which] = 0
+    return state_copy
 
+def xfer(state, max, source, dest):
+    # Make a copy of state
+    state_copy = state[:]
+    src = state_copy[source]
+    dst = state_copy[dest]
 
-# returns a copy of state which pours the contents of the jug at index source into the jug at index dest, until source is empty or dest is full.
-def xfer(state, maxCap, source, dest):
+    dst_remaining = max[dest] - dst
+    pour_amount = min(src, dst_remaining)
+    state_copy[source] -= pour_amount
+    state_copy[dest] += pour_amount
+    return state_copy
 
-	# copy of state
-	origin3 = list(state)
-	result3 = origin3
+def succ(state, max):
+    # fill() has two options: fill either left or right
+    s0 = fill(state, max, 0)
+    s1 = fill(state, max, 1)
 
-	# source is 0 and dest is 1
-	if source < dest:
-		if result3[source] + result3[dest] >= maxCap[dest]:
-			sub = maxCap[dest] - result3[dest]
-			result3[source] -= sub
-			result3[dest] = maxCap[dest]
-		else:
-			result3[dest] += result3[source]
-			result3[source] = 0
-	# source is 1 and dest is 0	
-	if source > dest:	
-		if result3[source] + result3[dest] >= maxCap[dest]:
-			sub = maxCap[dest] - result3[dest]
-			result3[source] -= sub	
-			result3[dest] = maxCap[dest]
+    # empty() has two options: empty either left or right
+    s2 = empty(state, max, 0)
+    s3 = empty(state, max, 1)
 
-		else:
-			result3[dest] += result3[source]
-			result3[source] = 0
+    # xfer() has two options: xfer from left to right, or xfer from right to left
+    s4 = xfer(state, max, 0, 1)
+    s5 = xfer(state, max, 1, 0)
 
-	return result3
+    # List of all possible states (which could have duplicates)
+    possible_states = [s0, s1, s2, s3, s4, s5]
 
+    # Remove duplicates
+    unique_states = []
+    for s_possible in possible_states:
+        if not _in_unique(s_possible, unique_states):
+            unique_states.append(s_possible)
 
-# prints the list of unique successor states of the current state in any order. 
-def succ(state, maxCap):
-	fill0 = fill(state, maxCap, 0)	
-	fill1 = fill(state, maxCap, 1)
-	empty0 = empty(state, maxCap, 0)
-	empty1 = empty(state, maxCap, 1)
-	xfer01 = xfer(state, maxCap, 0, 1)
-	xfer10 = xfer(state, maxCap, 1, 0) 
-
-	add = [fill0, fill1, empty0, empty1, xfer01, xfer10]
-
-	result = []
-	# Convert items to a tuple, then convert it the whole thing to a set, then convert everything back to a list:
-	result = list(set(tuple(item) for item in add))
-	print(result) 
+    # Print unique states
+    for s in unique_states:
+        print(s)
 
 
+# Helper function that returns True if new_item is already present in item_list, False otherwise.
+def _in_unique(new_item, item_list):
+    for item in item_list:
+        if new_item == item:
+            return True
+    return False
 
-# main	
-if __name__ == "__main__":
-	s0 = [0,0]
-	max = [14,7]
-	 
-#	print(fill(s0, max, 1))
-#	print(empty(s0, max, 0 ))
-#	print(s0)
-#	print(max)
-#	xfer(s0, max, 1, 0)
-	succ(s0, max)	
+
+def main():
+    s0 = [0, 0]
+    max = [5, 7]
+    print(fill(s0, max, 1))
+    print(fill(s0, max, 0))
+
+    s1 = fill(s0, max, 1)
+    print(xfer(s1, max, 1, 0))
+
+    succ(s0, max)
+
+    max = [5, 7]
+    s0 = [3, 1]
+    succ(s0, max)
+
+    s0 = [0, 0]
+    max = [3, 7]
+    print(s0 == empty(s0, max, 1))
+
+
+if __name__ == '__main__':
+    main()
